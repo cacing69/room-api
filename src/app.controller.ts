5680af69a0fefb0ea93148150d95fbfe4b26be19
cacing69 <ibnuul@gmail.com>
2022-12-07 15:59:57 +0700

    add ping

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { ResponseType, setResponse } from './core/helpers/response-helper';
+import { Controller, Delete, Get, Post, Put } from '@nestjs/common';
 import { AppService } from './app.service';
 import { Public } from './core/decorators/public.decorator';
 
@@ -10,5 +11,32 @@ export class AppController {
   @Public()
   getApp(): object {
     return this.appService.getResponse();
+  }
+
+  @Get('ping')
+  @Public()
+  getPing(): object {
+    return setResponse(ResponseType.Basic, { message: 'pong', method: 'get' });
+  }
+
+  @Post('ping')
+  @Public()
+  postPing(): object {
+    return setResponse(ResponseType.Basic, { message: 'pong', method: 'post' });
+  }
+
+  @Put('ping')
+  @Public()
+  putPing(): object {
+    return setResponse(ResponseType.Basic, { message: 'pong', method: 'put' });
+  }
+
+  @Delete('ping')
+  @Public()
+  deletePing(): object {
+    return setResponse(ResponseType.Basic, {
+      message: 'pong',
+      method: 'delete',
+    });
   }
 }
